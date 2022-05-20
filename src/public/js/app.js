fa586cd2440a8bed35701a884b01c2e24de3f545
gwdev <geonwoo.dev@gmail.com>
2022-05-20 23:14:53 +0900

    Nickname_part

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,16 +1,51 @@
-
-
+const messageList = document.querySelector("ul");
+const nickForm = document.querySelector("#nick");
+const messageForm = document.querySelector("#message");
+//서버와 connetion
 const socket = new WebSocket(`ws://${window.location.host}`);
 
-socket.addEventListener("open", () => {
+function makeMessage(type, payload){
+    const msg = { type, payload };
+    return JSON.stringify(msg);
+}
+
+
+
+function handleOpen() {
     console.log("Conneted to Server");
-});
+}
 
-socket.addEventListener("messge", (messge) => {
-    console.log.apply("Just got this: ", message, "from the server")
+socket.addEventListener("open", handleOpen);
 
+socket.addEventListener("message", (message) => {
+    const li = document.createElement("li");
+    li.innerText = message.data;
+    messageList.append(li);
 });
 
 socket.addEventListener("close", () => {
     console.log("Disconneted from Server");
 });
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const input = messageForm.querySelector("input");
+    socket.send(makeMessage("new_message", input.value));
+    input.value = "";
+
+    
+}
+
+function handleNickSubmit(event){
+    event.preventDefault();
+    const input = nickForm.querySelector("input");
+    socket.send(makeMessage("nickname", input.value));
+    input.value = "";
+}
+
+messageForm.addEventListener("submit", handleSubmit);
+nickForm.addEventListener("submit", handleNickSubmit)
+
+
+
+
--- src/server.js
+++ src/server.js
@@ -17,15 +17,30 @@ const handleListen = () => console.log('Listening on http://localhost:3000');
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
-function handleConnection(socket) { 
-    console.log(socket);
+
+function onSocketClose() {
+    console.log("Disconnected from the Browser");
 }
 
-wss.on("connection", (socket) => {
-    socket.send("hello!!!");
-    console.log("Conneted to Browser");
+const sockets = [];
+
+wss.on("connection", (socket) => {  //새로운 브라우저가 서버에 들어오면 실행
+    sockets.push(socket); //socket array에 저장
+    socket["nickname"] = "Anon"; //socket에 nickname 부여
+    console.log("Connected to Browser");
+    //서버에서 socket의 close 이벤트 발생, 페이지 종료 시 터미널에 메세지 출력
+    socket.on("close", onSocketClose);
+    socket.on("message", (msg) => { //socket이 메세지를 보낼 때까지 대기
+        const message = JSON.parse(msg);
+        switch (message.type) {
+            case "new_message":
+                sockets.forEach((aSocket)=>
+                aSocket.send(`${socket.nickname}: ${message.payload}`));
+            case "nickname":
+                socket["nickname"] = message.payload;
+        }
+    });
 });
-
-
-
+        
 server.listen(3000, handleListen);
+
